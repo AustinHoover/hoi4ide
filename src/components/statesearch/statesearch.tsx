0d36e3b4b82d2f53b1154c5a9e9987474b4e3ac9
AustinHoover <25714838+AustinHoover@users.noreply.github.com>
2024-10-07 14:41:12 -0400

    move state selection to standard dropdown

--- src/components/statesearch/statesearch.tsx
+++ src/components/statesearch/statesearch.tsx
@@ -5,6 +5,7 @@ import { createActionOpenState } from "../../state/mainState.actions";
 import { AppContextInterface, State } from "../../state/mainState.interface";
 
 import "./statesearch.css"
+import GenericDropdown from "../dropdowngeneric/dropdowngeneric";
 
 export interface CountrySearchProps {
     
@@ -18,41 +19,25 @@ const StateSearch = (props: CountrySearchProps) => {
     const state = context.state
     const dispatch = context.dispatch
 
-    let [showDropdownChildren, setShowDropdownChildren] = React.useState(false)
-    let [filterString, setFilterString] = React.useState("")
+    const states = state.projectDetails.stateEditing.states
+    const names: string[] = states.map(stateData => 
+        state.projectDetails.localisationMap[stateData.name] ? state.projectDetails.localisationMap[stateData.name] : stateData.name
+    )
 
-    let statesToInclude: State[] = []
-    statesToInclude = state.projectDetails.stateEditing.states.filter(stateData => stateData.name.toLocaleLowerCase().includes(filterString) || state.projectDetails.localisationMap[stateData.name].toLocaleLowerCase().includes(filterString))
-    if(statesToInclude.length > 5){
-        statesToInclude.length = 5
-    }
-
-    let filterFunction = (input: any) => {
-        setFilterString(input.target.value)
-        statesToInclude = state.projectDetails.stateEditing.states.filter(stateData => stateData.name.toLocaleLowerCase().includes(filterString) || state.projectDetails.localisationMap[stateData.name].toLocaleLowerCase().includes(filterString))
-        if(statesToInclude.length > 5){
-            statesToInclude.length = 5
+    const onSelect = (value: string) => {
+        const foundState: State | undefined = states.find(stateData => (state.projectDetails.localisationMap[stateData.name] ? state.projectDetails.localisationMap[stateData.name] : stateData.name) === value)
+        if(foundState){
+            dispatch(createActionOpenState(foundState))
         }
+        return value
     }
-
-    let contents: JSX.Element[] = []
-    statesToInclude.forEach(stateData => {
-        contents.push(
-            <a className="dropdown-item" key={stateData.name} onClick={(event) => {
-                dispatch(createActionOpenState(stateData))
-            }}>{state.projectDetails.localisationMap[stateData.name]} ({stateData.name})</a>
-        )
-    })
     
     return (
         <div className="dropdown mb-5 w-50">
-            {/* <a className="btn btn-secondary dropdown-toggle" onClick={()=>{setShowDropdownChildren(!showDropdownChildren)}}>
-                Dropdown link
-            </a> */}
-            <input className="m-2 form-control" type="text" placeholder="Search.." id="myInput" onKeyUp={filterFunction} onClick={()=>{setShowDropdownChildren(true)}}/>
-            <div className={`dropdown-menu w-100 m-2 ${showDropdownChildren ? "show" : ""}`} aria-labelledby="dropdownMenuLink">
-                {contents}
-            </div>
+            <GenericDropdown
+                options={names}
+                onSelect={onSelect}
+            />
         </div>
     );
 }
